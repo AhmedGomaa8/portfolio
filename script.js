959cd1e0807b45c9e468c5156247fd4cef3e22f4
Ahmed Gomaa <ahmedigomaa965@gmail.com>
2023-02-26 09:41:05 +0200

    update

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ let ulMenu = document.getElementById("ul-menu");
 menu.addEventListener("click", () => {
   ulMenu.classList.toggle("drop");
 });
+
+//slide
+
+let slides = document.querySelectorAll(".slide");
+let aboutSlides = document.querySelectorAll(".about-slide");
+let faders = document.querySelectorAll(".fade");
+let projectSlide = document.querySelectorAll(".project-slide");
+
+let option = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px",
+};
+
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      observer.unobserve(entry.target);
+    }
+  });
+}, option);
+
+slides.forEach((slide) => {
+  observer.observe(slide);
+});
+aboutSlides.forEach((slide) => {
+  observer.observe(slide);
+});
+faders.forEach((fade) => {
+  observer.observe(fade);
+});
+projectSlide.forEach((slide) => {
+  observer.observe(slide);
+});
